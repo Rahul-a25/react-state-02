43014729b799fce835a13f370b0280342c9d7268
unknown <rahularora993676@gmail.com>
2023-01-11 01:16:14 -0800

    state-02

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import ReactMarkdown from 'react-markdown';
+import { useState } from 'react';
+
+function App() {
+
+const [markdown,setMarkdown]=useState("")
+
+  return (
+    <div className="App">
+      <div className='div1'>
+        <textarea rows="10" cols="50" onChange={(e)=>setMarkdown(e.target.value)}/>
+      </div>
+      <div className='div2'>
+        <ReactMarkdown>
+          {markdown}
+        </ReactMarkdown>
+      </div>
+    </div>
+  );
+}
+
+export default App;
